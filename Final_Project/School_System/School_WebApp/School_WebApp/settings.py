a78f8cba878144aeb0ce550ed53003101fa7aad9
JCery <ozoriae4@gmail.com>
2023-09-13 22:17:10 +0200

    animacion de estudiantes cambiada

--- Final_Project/School_System/School_WebApp/School_WebApp/settings.py
+++ Final_Project/School_System/School_WebApp/School_WebApp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'school_system',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'melavasamascar22',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
